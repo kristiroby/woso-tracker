e88ecaa812783d0a1ef7586446ce100e2ad700b9
Kristi Roby <krtiroby@Andrews-MacBook-Pro.local>
2023-03-17 17:45:35 -0500

    render women and mens standings

---
+++ woso-tracker/src/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+import WomensTable from './Components/WomensTable'
+import MensTable from './Components/MensTable'
+
+function App() {
+  return (
+    <div className="App">
+      < WomensTable />
+      < MensTable />
+    </div>
+  );
+}
+
+export default App;
